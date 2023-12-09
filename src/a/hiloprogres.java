d900d0799a5a3ddc769d36c5ba28ded6cce9ab4f
Diego122004 <daof.04@mail.com>
2023-12-08 19:09:24 -0600

    hellllo2

--- src/a/hiloprogres.java
+++ src/a/hiloprogres.java
@@ -1,7 +1,62 @@
-
 package a;
 
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JProgressBar;
 
 public class hiloprogres {
-    
+
+    private JProgressBar barra;
+    private boolean avanzar;
+    private boolean viva;
+
+    public hiloprogres(JProgressBar barra) {
+        this.barra = barra;
+        avanzar = true;
+        viva = true;
+
+    }
+
+    public JProgressBar getBarra() {
+        return barra;
+    }
+
+    public void setBarra(JProgressBar barra) {
+        this.barra = barra;
+    }
+
+    public boolean isAvanzar() {
+        return avanzar;
+    }
+
+    public void setAvanzar(boolean avanzar) {
+        this.avanzar = avanzar;
+    }
+
+    public boolean isViva() {
+        return viva;
+    }
+
+    public void setViva(boolean viva) {
+        this.viva = viva;
+    }
+
+    public void run() {
+        if (viva) {
+
+            barra.setForeground(Color.red);
+            barra.setMaximum(12);
+            if (barra.getValue() == 12) {
+                viva = false;
+            }
+
+            try {
+                Thread.sleep(500 );
+            } catch (InterruptedException ex) {
+                Logger.getLogger(hiloprogres.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
 }
